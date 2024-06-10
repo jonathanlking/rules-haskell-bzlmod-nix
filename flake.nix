07914d67230192a24c624b23f6f27d7a4b624162
Jonathan King <jonathanlking@me.com>
2024-06-10 15:27:36 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A flake for setting up a dev shell with Bazel";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
+    devShells.default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      buildInputs = [
+        pkgs.bazel
+      ];
+    };
+  });
+}
