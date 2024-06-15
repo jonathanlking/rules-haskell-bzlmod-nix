7bff12d604528152e45313d12236f029979d1e0e
Jonathan King <jonathanlking@me.com>
2024-06-15 17:30:51 +0100

    Simplify example

--- Example.hs
+++ Example.hs
@@ -1,10 +1,8 @@
 module Main where
 
-import Prelude
-import Data.Set qualified as Set
 import Codec.Compression.Zlib (compress, decompress)
 import Prelude ((.), putStrLn)
 
-main = print (Set.singleton "Hello from rules_haskell!")
+main = putStrLn "Hello from rules_haskell!"
 
 slowId = decompress . compress
